408009b2e89aaa540ee33570c34f35998c49722a
Miles Hill <miles.hill@corva.ai>
2020-05-19 17:21:19 -0500

    Add geojson for Streamlit

--- marcellus/app.py
+++ marcellus/app.py
@@ -5,13 +5,12 @@ import pymongo
 import streamlit as st
 
 
-
 # Connect to database
 connection = pymongo.MongoClient(host="localhost", port=27017)
 collection = connection["marcellus"]["report.production"]
 
 # Load the county boundaries
-with open("/home/miles/Downloads/Pennsylvania County Boundaries.geojson") as fin:
+with open("Pennsylvania County Boundaries.geojson") as fin:
     counties = json.load(fin)
 
 # Load all unique periods
@@ -22,23 +21,14 @@ st.title("Marcellus Gas Well by County")
 
 # Get a dataframe with elements from the first periods
 def get_data_by_period(period):
-    cursor = collection.aggregate([
-        {
-            "$match": {
-                "production_report.period": period
-            }
-        },
-        {
-            "$group": {
-                "_id": "$county",
-                "sum": {"$sum": 1}
-            }
-        }
-    ])
+    cursor = collection.aggregate(
+        [{"$match": {"production_report.period": period}}, {"$group": {"_id": "$county", "sum": {"$sum": 1}}}]
+    )
     records = ({"county": r.get("_id"), "sum": r.get("sum")} for r in cursor)
     df = pd.DataFrame.from_dict(records)
     df["county"] = df["county"].str.upper()
     return df
+
 
 # Create a sidebar
 option_period = st.sidebar.selectbox("Period", periods)
@@ -56,13 +46,12 @@ def create_map_from_period(option_period):
             featureidkey="properties.county_nam",
             color="sum",
             color_continuous_scale="Viridis",
-            scope="usa"
+            scope="usa",
         )
         fig.update_geos(fitbounds="locations")
-        fig.update_layout(
-            margin=dict(r=0, l=0, t=0, b=0)
-        )
+        fig.update_layout(margin=dict(r=0, l=0, t=0, b=0))
     return fig
+
 
 def create_barchart_from_period(option_period):
     with st.spinner("Getting data..."):
